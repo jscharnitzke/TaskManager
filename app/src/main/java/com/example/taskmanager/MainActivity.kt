5f7c600e66881eb5254363009b9052cadbd960dd
Joe Scharnitzke <j.scharnitzke@gmail.com>
2023-12-30 15:04:55 -0500

    add unstyled components for each UI element

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
@@ -22,11 +25,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TasksRemainingText()
+                    ContentBody()
                 }
             }
         }
     }
+}
+
+@Composable
+fun CheckmarkImage(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+
+    Image(
+        contentDescription = null,
+        modifier = modifier,
+        painter = image,
+    )
+}
+
+@Composable
+fun ContentBody(modifier: Modifier = Modifier) {
+    Column {
+        CheckmarkImage(modifier = modifier)
+        TasksRemainingText(modifier = modifier)
+        PraiseText(modifier = modifier)
+    }
+}
+
+@Composable
+fun PraiseText(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        text = "Nice work!",
+    )
 }
 
 @Composable
@@ -41,6 +72,6 @@ fun TasksRemainingText(modifier: Modifier = Modifier) {
 @Composable
 fun TaskManagerPreview() {
     TaskManagerTheme {
-        TasksRemainingText()
+        ContentBody()
     }
 }
