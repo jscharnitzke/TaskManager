71673bb74ebd35073d404b82661a20511280d866
Joe Scharnitzke <j.scharnitzke@gmail.com>
2023-12-30 15:08:56 -0500

    add styling to all elements

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +52,10 @@ fun CheckmarkImage(modifier: Modifier = Modifier) {
 
 @Composable
 fun ContentBody(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
         CheckmarkImage(modifier = modifier)
         TasksRemainingText(modifier = modifier)
         PraiseText(modifier = modifier)
@@ -57,6 +66,7 @@ fun ContentBody(modifier: Modifier = Modifier) {
 fun PraiseText(modifier: Modifier = Modifier) {
     Text(
         modifier = modifier,
+        fontSize = 16.sp,
         text = stringResource(R.string.praise_text),
     )
 }
@@ -64,7 +74,8 @@ fun PraiseText(modifier: Modifier = Modifier) {
 @Composable
 fun TasksRemainingText(modifier: Modifier = Modifier) {
     Text(
-        modifier = modifier,
+        modifier = modifier.padding(bottom = 8.dp, top = 24.dp),
+        fontWeight = FontWeight.Bold,
         text = stringResource(R.string.tasks_remaining_text),
     )
 }
